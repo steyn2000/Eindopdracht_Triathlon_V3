ea4a42721d9eb6ac7bfbaa36d9d59fa658c77ddf
steyn2000 <steyn.schippers@kpnmail.nl>
2025-09-29 21:09:32 +0200

    1

--- Eindopdracht_Triathlon_V3.cpp
+++ Eindopdracht_Triathlon_V3.cpp
@@ -14,7 +14,6 @@
 #include "Deelnemer.h"
 #include "Wedstrijd.h"
 #include "Licentie.h"
-#include "valide_datum.h"
 
 using namespace std;
 
@@ -182,6 +181,43 @@ void print_keuzemenu()
     cout << "8. Dopingcontrole toevoegen\n";
     cout << "9. Uitslagen tonen\n";
     cout << "10. Stoppen\n"; // De terminal bleef zwart, menu niet zichtbaar terwijl de code wel leek te werken
+}
+
+/**
+ * @brief Controleert of een datum in het formaat dd-mm-jjjj geldig is.
+ * @param datum Te controleren datumstring.
+ * @return True als de datum geldig is, anders false.
+ */
+bool valide_datum(const string& datum)
+{
+    if (datum.size() != 10 || datum[2] != '-' || datum[5] != '-')
+        return false;
+    for (size_t datum_index = 0; datum_index < datum.size(); ++datum_index)
+    {
+        if (datum_index == 2 || datum_index == 5)
+            continue;
+        char c = datum[datum_index];
+        if (c < '0' || c > '9')
+            return false;
+    }
+    int dag = (datum[0] - '0') * 10 + (datum[1] - '0');
+    int maand = (datum[3] - '0') * 10 + (datum[4] - '0');
+    int jaar = (datum[6] - '0') * 1000 + (datum[7] - '0') * 100 + (datum[8] - '0') * 10 + (datum[9] - '0');
+    if (jaar < 1900 || jaar > 2100)
+        return false;
+    if (maand < 1 || maand > 12)
+        return false;
+    int dagen_per_maand[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+    int dagen = dagen_per_maand[maand - 1];
+    if (maand == 2)
+    {
+        bool leap = (jaar % 4 == 0 && (jaar % 100 != 0 || jaar % 400 == 0));
+        if (leap)
+            dagen = 29;
+    }
+    if (dag < 1 || dag > dagen)
+        return false;
+    return true;
 }
 
 // datum helpers voor "dd-mm-jjjj"
